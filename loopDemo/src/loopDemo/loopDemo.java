72fab74f6d44e4fb818ec9cad01389ee20b947a1
Umut <umutkkozan@gmail.com>
2022-10-12 00:34:58 +0300

    first commit

---
+++ loopDemo/src/loopDemo/loopDemo.java
@@ -0,0 +1,31 @@
+package loopDemo;
+
+public class loopDemo {
+
+	public static void main(String[] args) {
+		
+		
+	//	for(int i=1; i=10; i++);
+	//	{ 
+	//		System.out.println("Sayi"+ );
+	//		}
+	   // int i =1;
+	  // while (i<10) {
+		//System.out.println("İ Döngüsü: "+i );   
+	//	i++;
+	  // }
+	 //  System.out.println( "While Döngüsü Bitti "+i +" a kadar sayıldı");
+        
+		//Do While Döngüsü 
+  	   int j=100;
+	   do {
+		   System.out.println(j);
+		   j+=2;
+	   }
+	   while (j<10);
+	   System.out.println("Do-While Döngüsü Bitti");
+	   
+	   
+	}
+
+}
